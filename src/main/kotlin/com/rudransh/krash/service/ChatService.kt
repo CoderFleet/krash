95790ac4ae6ed07cebb1b88fd6279d45fccdfc23
Rudransh Pratap Singh <tosylfluoride@gmail.com>
2026-03-27 18:58:55 +0530

    Basic Broadcast Chat Working

--- src/main/kotlin/com/rudransh/krash/service/ChatService.kt
+++ src/main/kotlin/com/rudransh/krash/service/ChatService.kt
@@ -4,17 +4,24 @@ import com.rudransh.krash.chat.*
 import kotlinx.coroutines.channels.SendChannel
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.buffer
 import kotlinx.coroutines.flow.channelFlow
 import org.springframework.stereotype.Service
+import java.util.concurrent.CopyOnWriteArrayList
 
 @Service
 class ChatService : ChatServiceGrpcKt.ChatServiceCoroutineImplBase() {
-    private val clients = mutableListOf<SendChannel<ChatMessage>>()
+    private val clients = CopyOnWriteArrayList<SendChannel<ChatMessage>>()
 
     override suspend fun sendMessage(request: ChatMessage): Empty {
         println("Message from ${request.sender}: ${request.content}")
 
-        clients.forEach { it.send(request) }
+        clients.forEach { client -> try {
+                client.trySend(request)
+            } catch(e: Exception) {
+                println("Failed to send message to client")
+            }
+        }
 
         return Empty.getDefaultInstance()
     }
@@ -28,5 +35,5 @@ class ChatService : ChatServiceGrpcKt.ChatServiceCoroutineImplBase() {
             println("${request.username} disconnected")
             clients.remove(channel)
         }
-    }
+    }.buffer()
 }
